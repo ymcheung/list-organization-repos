86f2b1e1eb8b9b59c48390535d4ae3069c0b337f
ymcheung <ymcheung.tw@me.com>
2022-04-26 18:09:53 +0800

    Setup handleFetchData

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
+interface FetchProps {
+  (type: string): void;
+}
+interface Data {
+  id: string;
+  html_url: string;
+  name: string;
+}
+
 function App() {
+  const [repos, setRepos] = useState({
+    data: [],
+    loading: true
+  });
+
+  const handleFetchData: FetchProps = async(type) => {
+    let url = `https://api.github.com/orgs/vercel/repos?type=${type}`;
+
+    try {
+      const res = await fetch(url);
+      const json = await res.json();
+
+      setRepos({data: json, loading: false});
+    } catch (error) {
+      console.log('error', error);
+    }
+}
+
+  useEffect(() => {
+    handleFetchData('forks');
+  }, [setRepos]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {
+          repos.loading? '' : repos.data.map(({id, html_url, name}: Data) => (
+          <li key={id}>
+            <a href={html_url}>{name}</a>
+          </li>
+          ))
+        }
+      </ul>
     </div>
   );
 }
