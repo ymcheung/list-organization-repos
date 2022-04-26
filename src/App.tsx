4ff119a75d753be5ae9dbe1663c4868ab9bec75e
ymcheung <ymcheung.tw@me.com>
2022-04-26 20:23:54 +0800

    Add filter doms

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,10 @@ function App() {
       const res = await fetch(url);
       const json = await res.json();
 
-      setRepos({data: json, loading: false});
+      setRepos({
+        data: json,
+        loading: false
+      });
     } catch (error) {
       console.log('error', error);
     }
@@ -35,6 +38,26 @@ function App() {
 
   return (
     <div className="App">
+      <header className="filter">
+        <fieldset>
+          <legend>Repos Type</legend>
+          <input id="type_all" type="radio" name="type" value="all" checked />
+          <label htmlFor="type_all">All</label>
+          <input id="type_forks" type="radio" name="type" value="forks" />
+          <label htmlFor="type_forks">Forks</label>
+        </fieldset>
+        <fieldset>
+          <legend>Sort</legend>
+          <input id="sort_created" type="radio" name="sort" value="created" checked />
+          <label htmlFor="sort_created">Created</label>
+          <input id="sort_updated" type="radio" name="sort" value="updated" />
+          <label htmlFor="sort_updated">Updated</label>
+          <input id="sort_pushed" type="radio" name="sort" value="pushed" />
+          <label htmlFor="sort_pushed">Pushed</label>
+          <input id="sort_full_name" type="radio" name="sort" value="full_name" />
+          <label htmlFor="sort_full_name">Full Name</label>
+        </fieldset>
+      </header>
       <ul>
         {
           repos.loading? '' : repos.data.map(({id, html_url, name}: Data) => (
